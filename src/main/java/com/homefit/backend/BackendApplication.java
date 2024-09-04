d099510535a3e7e767b1177b4196e8e11c847948
bellrajin <parkjin931@gmail.com>
2024-09-04 22:20:36 +0900

    Refactor: #4- baseEntity 로직 수정 작업.

--- src/main/java/com/homefit/backend/BackendApplication.java
+++ src/main/java/com/homefit/backend/BackendApplication.java
@@ -5,12 +5,14 @@ import com.homefit.backend.login.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableConfigurationProperties({
 		CorsProperties.class,
 		AppProperties.class
 })
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApplication {
 
 	public static void main(String[] args) {

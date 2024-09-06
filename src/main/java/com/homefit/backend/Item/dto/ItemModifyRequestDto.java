55e3510ed83827542a26126bd5ab0dedd7befdf1
Cheong Ye Jun <jjyejun@naver.com>
2024-09-06 18:17:41 +0900

    Feat: 아이템 수정용 Dto 구현

--- src/main/java/com/homefit/backend/Item/dto/ItemModifyRequestDto.java
+++ src/main/java/com/homefit/backend/Item/dto/ItemModifyRequestDto.java
@@ -1,10 +1,14 @@
 package com.homefit.backend.Item.dto;
 
+import com.homefit.backend.itemcategory.entity.ItemCategory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ItemModifyRequestDto {
+
+    private ItemCategory itemCategory;
 }

f318d17c731c4bad5c6bac98aee889b039c79752
Cheong Ye Jun <jjyejun@naver.com>
2024-09-06 17:50:15 +0900

    Add: 개발용 더미 데이터 추가

---
+++ src/main/java/com/homefit/backend/Item/controller/ItemController.java
@@ -0,0 +1,12 @@
+package com.homefit.backend.Item.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+
+
+}

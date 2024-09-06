c8a275a3a5a9173946c9a6c73f9a0fe2d75c943f
Cheong Ye Jun <jjyejun@naver.com>
2024-09-06 16:48:57 +0900

    Feat: 아이템 엔티티 구현

---
+++ src/main/java/com/homefit/backend/Item/entity/Item.java
@@ -0,0 +1,26 @@
+package com.homefit.backend.Item.entity;
+
+import com.homefit.backend.itemcategory.entity.ItemCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id"})
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_category_id") // FK
+    private ItemCategory itemCategory;
+}

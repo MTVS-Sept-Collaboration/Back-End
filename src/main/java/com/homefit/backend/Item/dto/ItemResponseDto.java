ac26223b401623d87b951d2467337023c7ddb6af
Cheong Ye Jun <jjyejun@naver.com>
2024-09-06 17:50:15 +0900

    Add: 개발용 더미 데이터 추가

---
+++ src/main/java/com/homefit/backend/Item/dto/ItemResponseDto.java
@@ -0,0 +1,10 @@
+package com.homefit.backend.Item.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ItemResponseDto {
+}

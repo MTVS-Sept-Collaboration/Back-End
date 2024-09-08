2eb342cab69f87fd27fb296e1e9e4f1218c6a7f2
LooanCheong <jjyejun@naver.com>
2024-09-08 16:17:01 +0900

    Feat: 아이템 단일 조회용 DTO 구현

--- src/main/java/com/homefit/backend/item/dto/ItemResponseDto.java
+++ src/main/java/com/homefit/backend/item/dto/ItemResponseDto.java
@@ -1,9 +1,15 @@
 package com.homefit.backend.item.dto;
 
+import com.homefit.backend.itemcategory.entity.ItemCategory;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
+@Setter
 public class ItemResponseDto {
+
+    private Long id;
+    private ItemCategory category;
 }

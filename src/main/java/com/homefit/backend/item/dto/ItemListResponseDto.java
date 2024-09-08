2abe205176ef6c0c6859cd0f358bf975d7f55fda
LooanCheong <jjyejun@naver.com>
2024-09-08 16:28:45 +0900

    Feat: DTO AllArg 생성자 추가

--- src/main/java/com/homefit/backend/item/dto/ItemListResponseDto.java
+++ src/main/java/com/homefit/backend/item/dto/ItemListResponseDto.java
@@ -1,9 +1,16 @@
 package com.homefit.backend.item.dto;
 
+import com.homefit.backend.itemcategory.entity.ItemCategory;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ItemListResponseDto {
+
+    private Long id;
+    private ItemCategory itemCategory;
+
 }
